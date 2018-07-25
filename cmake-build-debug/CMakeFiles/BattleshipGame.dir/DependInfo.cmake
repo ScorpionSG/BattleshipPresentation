15c0cc14eb95f73a7c6c930b3393d6ffed9b67cb
AE <karimizin@gmail.com>
2018-07-25 18:38:05 -0500

    Let's try this again

---
+++ cmake-build-debug/CMakeFiles/BattleshipGame.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/alielnour/CLionProjects/BattleshipGame/main.cpp" "/Users/alielnour/CLionProjects/BattleshipGame/cmake-build-debug/CMakeFiles/BattleshipGame.dir/main.cpp.o"
+  "/Users/alielnour/CLionProjects/BattleshipGame/src/Board.cpp" "/Users/alielnour/CLionProjects/BattleshipGame/cmake-build-debug/CMakeFiles/BattleshipGame.dir/src/Board.cpp.o"
+  "/Users/alielnour/CLionProjects/BattleshipGame/src/Cursor.cpp" "/Users/alielnour/CLionProjects/BattleshipGame/cmake-build-debug/CMakeFiles/BattleshipGame.dir/src/Cursor.cpp.o"
+  "/Users/alielnour/CLionProjects/BattleshipGame/src/Ship.cpp" "/Users/alielnour/CLionProjects/BattleshipGame/cmake-build-debug/CMakeFiles/BattleshipGame.dir/src/Ship.cpp.o"
+  "/Users/alielnour/CLionProjects/BattleshipGame/src/Tile.cpp" "/Users/alielnour/CLionProjects/BattleshipGame/cmake-build-debug/CMakeFiles/BattleshipGame.dir/src/Tile.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/BattleshipGame.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BattleshipGame.dir/main.cpp.o"
+  "CMakeFiles/BattleshipGame.dir/src/Board.cpp.o"
+  "CMakeFiles/BattleshipGame.dir/src/Tile.cpp.o"
+  "CMakeFiles/BattleshipGame.dir/src/Cursor.cpp.o"
+  "CMakeFiles/BattleshipGame.dir/src/Ship.cpp.o"
+  "BattleshipGame.pdb"
+  "BattleshipGame"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BattleshipGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
